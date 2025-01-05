17a54bf7225f94329948602e923190200a3ce459
Hedvig Reuterswärd <hedvigreutersward@Hedvigs-MacBook-Pro-2.local>
2025-01-05 01:41:14 +0100

    Adding TextQuiz and FlagQuiz as additive scenes when pressing debug buttons, updating FlagQuizScene

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -9,11 +9,27 @@ public class BoardController : MonoBehaviour
         StartFlagQuiz();
     }
     
+    public void OnTextQuizPressed()
+    {
+        StartTextQuiz();
+    }
+    
     private async void StartFlagQuiz()
     {
         Debug.Log("FlagQuiz: Showing");
 
         var loadOperation = SceneManager.LoadSceneAsync("FlagQuiz", LoadSceneMode.Additive);
+        while (!loadOperation.isDone)
+        {
+            await Task.Yield();
+        }
+    }
+    
+    private async void StartTextQuiz()
+    {
+        Debug.Log("FlagQuiz: Showing");
+
+        var loadOperation = SceneManager.LoadSceneAsync("TextQuiz", LoadSceneMode.Additive);
         while (!loadOperation.isDone)
         {
             await Task.Yield();
