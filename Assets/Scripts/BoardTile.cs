7c2037b1b2bc8efa81311637b361c8dbb621bec5
hedvig <hedvig.reutersward@gmail.com>
2025-01-06 17:01:12 +0100

    Changing color of material based upon tileType. Tweaking tile press color swap functionality to automatically lighten the base color. BoardTile reordering, cleanup

--- Assets/Scripts/BoardTile.cs
+++ Assets/Scripts/BoardTile.cs
@@ -11,36 +11,78 @@ public enum TileType
 
 public class BoardTile : MonoBehaviour
 {
+    [SerializeField]
+    private MeshRenderer tileRenderer;
+    [SerializeField] 
+    private TileType tileType;
+    
+    [Header("Color settings")]
+    [SerializeField] 
+    private Color defaultTileColor;
+    [SerializeField] 
+    private Color flagTileColor;
+    [SerializeField] 
+    private Color pictureTileColor;
+    [SerializeField]
+    private float pressLightenFactor;
+    
+    [Header("Animation settings")]
     [SerializeField] 
     private float pressDuration = 0.25f;
     [SerializeField]
     private float pressDownScale = 0.2f;
-    [SerializeField]
-    private MeshRenderer tileRenderer;
-    [SerializeField]
-    private Color pressColor = Color.white;
-    [SerializeField] 
-    private TileType tileType;
     
-    private MaterialPropertyBlock propertyBlock;
-    private Color originalColor;
-
+    private MaterialPropertyBlock _propertyBlock;
+    private Color _originalColor;
     private const string ShaderBaseColor = "_BaseColor";
 
     private void Awake()
     {
         // Use a material property block to swap colors of the material of an instance of the tile effectively
-        propertyBlock = new MaterialPropertyBlock();
-        tileRenderer.GetPropertyBlock(propertyBlock);
-        originalColor = propertyBlock.HasProperty(ShaderBaseColor) 
-            ? propertyBlock.GetColor(ShaderBaseColor) 
-            : tileRenderer.sharedMaterial.HasProperty(ShaderBaseColor) 
-                ? tileRenderer.sharedMaterial.GetColor(ShaderBaseColor) 
-                : Color.white;
+        _propertyBlock = new MaterialPropertyBlock();
+        tileRenderer.GetPropertyBlock(_propertyBlock);
     }
 
-  public void Hop()
+    private void Start()
+    {
+        ChangeTileColor(tileType);
+    }
+
+    /// <summary>
+    /// Change the tile type
+    /// </summary>
+    /// <param name="newTileType">Tile type we want to change to</param>
+    public void ChangeTileType(TileType newTileType)
+    {
+        tileType = newTileType;
+        ChangeTileColor(newTileType);
+    }
+    private void ChangeTileColor(TileType newTileType)
+    {
+        switch (newTileType)
+        {
+            case TileType.Default:
+                _propertyBlock.SetColor(ShaderBaseColor, defaultTileColor);
+                break;
+            case TileType.FlagQuiz:
+                _propertyBlock.SetColor(ShaderBaseColor, flagTileColor);
+                break;
+            case TileType.PictureQuiz:
+                _propertyBlock.SetColor(ShaderBaseColor, pictureTileColor);
+                break;
+            default:
+                Debug.LogWarning("Unknown tile type");
+                break;
+        }
+        tileRenderer.SetPropertyBlock(_propertyBlock);
+    }
+    public void Hop()
   {
+      _originalColor = _propertyBlock.HasProperty(ShaderBaseColor) 
+          ? _propertyBlock.GetColor(ShaderBaseColor) 
+          : tileRenderer.sharedMaterial.HasProperty(ShaderBaseColor) 
+              ? tileRenderer.sharedMaterial.GetColor(ShaderBaseColor) 
+              : Color.white;
       TileEffectAnimation();
   }
     private void TileEffectAnimation()
@@ -48,7 +90,7 @@ public class BoardTile : MonoBehaviour
         DOTween.To(
                 () => GetCurrentColor(),
                 x => SetTileColor(x),
-                pressColor,
+                LightenColor(_originalColor, pressLightenFactor),
                 pressDuration
             )
             .SetLoops(2, LoopType.Yoyo)
@@ -61,21 +103,7 @@ public class BoardTile : MonoBehaviour
         tileRenderer.transform.DOScaleY(currentScaleY, pressDuration)
             .SetEase(Ease.OutBounce);
     }
-
-    private Color GetCurrentColor()
-    {
-        tileRenderer.GetPropertyBlock(propertyBlock);
-        return propertyBlock.HasProperty(ShaderBaseColor) 
-            ? propertyBlock.GetColor(ShaderBaseColor) 
-            : originalColor;
-    }
-
-    private void SetTileColor(Color color)
-    {
-        propertyBlock.SetColor(ShaderBaseColor, color);
-        tileRenderer.SetPropertyBlock(propertyBlock);
-    }
-
+    
     public void Land()
     {
         Debug.Log($"Landed on {gameObject.name} with type {tileType})");
@@ -91,5 +119,24 @@ public class BoardTile : MonoBehaviour
             default:
                 throw new ArgumentOutOfRangeException();
         }
+    }
+    
+    private Color GetCurrentColor()
+    {
+        tileRenderer.GetPropertyBlock(_propertyBlock);
+        return _propertyBlock.HasProperty(ShaderBaseColor) 
+            ? _propertyBlock.GetColor(ShaderBaseColor) 
+            : _originalColor;
+    }
+
+    private void SetTileColor(Color color)
+    {
+        _propertyBlock.SetColor(ShaderBaseColor, color);
+        tileRenderer.SetPropertyBlock(_propertyBlock);
+    }
+    
+    private Color LightenColor(Color color, float factor)
+    {
+        return Color.Lerp(color, Color.white, factor);
     }
 }
