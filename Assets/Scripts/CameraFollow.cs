e29c57a927a1b76a52b7d3f99c2a1721079e5e42
hedvig <hedvig.reutersward@gmail.com>
2025-01-06 16:19:14 +0100

    Making simple camera script to follow player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float followDuration = 0.5f;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void Update()
+    {
+        Vector3 targetPosition = new Vector3(target.position.x + offset.x, transform.position.y, target.position.z + offset.z);
+        transform.DOMove(targetPosition, followDuration).SetEase(Ease.InOutQuad);
+    }
+}
