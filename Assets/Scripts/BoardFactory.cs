acf09ba2e2c4514fa7b05a6461c94a8cd05057b4
hedvig <hedvig.reutersward@gmail.com>
2025-01-06 18:48:54 +0100

    Randomise types of tiles on Start via BoardFactory

--- Assets/Scripts/BoardFactory.cs
+++ Assets/Scripts/BoardFactory.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Linq;
+using ModestTree;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 public interface IBoardFactory
 {
-    public void GenerateBoard();
+    public void AssignTileTypes();
 }
-
 
 public class BoardFactory : MonoBehaviour, IBoardFactory
 {
@@ -11,11 +15,38 @@ public class BoardFactory : MonoBehaviour, IBoardFactory
     [SerializeField] 
     private BoardTile[] tiles;
 
+    private TileType[] _allTileTypes;
+
     public BoardTile[] GetTiles()
     {
         return tiles;
     }
 
-    public void GenerateBoard()
-    { }
+    public void Start()
+    {
+        _allTileTypes = (TileType[])Enum.GetValues(typeof(TileType));
+        AssignTileTypes();
+    }
+
+    public void AssignTileTypes()
+    {
+        TileType previousType = new TileType();
+        foreach (var tile in tiles)
+        {
+            var randomType = _allTileTypes[Random.Range(0, _allTileTypes.Length)];
+            if (randomType == previousType)
+            {
+                randomType = GetNewRandomTileType(randomType);
+            }
+            tile.ChangeTileType(randomType);
+            previousType = randomType;
+        }
+    }
+    
+    private TileType GetNewRandomTileType(TileType typeToAvoid)
+    {
+        TileType[] arrayWithoutTypeToAvoid = _allTileTypes.Except(typeToAvoid).ToArray();
+        TileType newType = arrayWithoutTypeToAvoid[Random.Range(0, arrayWithoutTypeToAvoid.Length)];
+        return newType;
+    }
 }
