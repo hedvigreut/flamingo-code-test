70758fbf63430c94f65494d15c1ad1a39b38c478
hedvig <hedvig.reutersward@gmail.com>
2025-01-07 03:11:12 +0100

    Adding splash screen with animation and async loading progress bar

--- Assets/Scripts/SplashController.cs
+++ Assets/Scripts/SplashController.cs
@@ -1,21 +1,47 @@
-using System.Threading.Tasks;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SplashController : MonoBehaviour
 {
-    private async void Awake()
+    [SerializeField] 
+    private Slider progressBar;
+    [SerializeField] 
+    private TextMeshProUGUI progressBarText;
+    [SerializeField] 
+    private float fakeLoadingScalar = 20f;
+    
+    private void Start()
     {
-        await StartSplashScreen();
+        StartCoroutine(LoadSceneRoutine("Board"));
     }
 
-    private async Task StartSplashScreen()
+    private IEnumerator LoadSceneRoutine(string sceneName)
     {
-        Debug.Log("Splash: Showing");
-
-        // Simulate loading delay (e.g., a progress bar in the future)
-        await Task.Delay(2000);
-        SceneManager.LoadScene("Board", LoadSceneMode.Single);
-        Debug.Log("Splash: Transitioning to BoardScene");
+        float fakeProgress = 0f;
+        progressBar.value = 0f;
+        progressBarText.text = "0%";
+        
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        asyncOperation.allowSceneActivation = false;
+        while (!asyncOperation.isDone)
+        {
+            if (fakeProgress < 100f)
+            {
+                fakeProgress +=  Time.deltaTime * fakeLoadingScalar;
+                progressBar.value = fakeProgress / 100f;
+                progressBarText.text = Mathf.Clamp(fakeProgress, 0, 100).ToString("F0") + "%";
+            }
+            if (fakeProgress >= 90f && asyncOperation.progress >= 0.9f)
+            {
+                fakeProgress = 100f;
+                progressBar.value = 1f;
+                progressBarText.text = "100%";
+                asyncOperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
     }
 }
