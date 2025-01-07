fea49884a8244003f8697e35086303ff2f69bdf9
hedvig <hedvig.reutersward@gmail.com>
2025-01-07 01:56:08 +0100

    SImple fade in/out on travel points when landing on default tile

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
     private ParticleSystem particleSystem;
+    [SerializeField] 
+    private TMP_Text travelPointText;
     public void PlayCoinEffect()
     {
         particleSystem.Play();
     }
-
     public void PlayTravelPointsEffect(int coinValue)
     {
-        Debug.Log("Play travel points");
+        travelPointText.text = $"+{coinValue}";
+        travelPointText.gameObject.SetActive(true);
+        travelPointText.DOFade(1, 1f)
+            .OnKill(() => travelPointText.DOFade(0, 1f)
+                .OnKill(() => travelPointText.gameObject.SetActive(false)));
     }
 }
